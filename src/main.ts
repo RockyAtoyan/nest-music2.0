f74e1710739e7bc798f7fd89b5d435f69db563f2
Rocky <atoyanrobert21@gmail.com>
2024-07-13 15:08:37 +0300

    add origin

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     credentials: true,
-    origin: ['http://localhost:3000'],
+    origin: ['http://localhost:3000', 'https://music2-0-black.vercel.app'],
   });
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
