9e25413c8e8681112003501a1c1eda1acbd8f0de
Rocky <atoyanrobert21@gmail.com>
2024-07-13 17:20:44 +0300

    add origin

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,11 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     credentials: true,
-    origin: ['http://localhost:3000', 'https://music2-0-black.vercel.app'],
+    origin: [
+      'http://localhost:3000',
+      'https://music2-0-black.vercel.app',
+      'https://rockyatoyan-musichub.vercel.app',
+    ],
   });
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
